94f9119f37f8741a973fc6130c7400dacd29e5bc
Oscar A. Rangel <orangelca@gmail.com>
2019-02-09 14:08:38 -0500

    Better Result with Keras-GPU than pytorch-GPU
    Epoch 12/12
    60000/60000 [==============================] - 6s 96us/step - loss: 0.0285 - acc: 0.9914 - val_loss: 0.0289 - val_acc: 0.9908
    Test loss: 0.028865928596209414
    Test accuracy: 0.9908 - very good
    Using GPU NVIDA 1070 6 Gbytes
    6 seconds per epoch
    '''

---
+++ mnist_keras.py
@@ -0,0 +1,86 @@
+'''
+Epoch 12/12
+60000/60000 [==============================] - 6s 96us/step - loss: 0.0285 - acc: 0.9914 - val_loss: 0.0289 - val_acc: 0.9908
+Test loss: 0.028865928596209414
+Test accuracy: 0.9908 - very good
+Using GPU NVIDA 1070 6 Gbytes
+6 seconds per epoch
+'''
+
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+# in order to use GPU we need to install the keras GPU version
+# https://pypi.org/project/tensorflow-gpu/#history
+# and set it to use GPU
+import tensorflow as tf
+config = tf.ConfigProto( device_count = {'GPU': 1 , 'CPU': 56} ) 
+sess = tf.Session(config=config) 
+keras.backend.set_session(sess)
+
+# if tf.test.gpu_device_name():
+#     print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+# else:
+#     print("Please install GPU version of TF")
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# create our model
+# created model with Conv2d *2 =>MaxPool => DropOut => Flaten => FC => Drop =>Softmax
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+# train.
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
